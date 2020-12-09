61a1a296cec4316e734a154546b22f4863fead3f
Christopher Corvo <christophercorvo@gmail.com>
2020-12-08 19:35:17 -0500

    initial commit

---
+++ src/main/java/com/lambdaschool/getjavaorders/GetjavaordersApplication.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.getjavaorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetjavaordersApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GetjavaordersApplication.class,
+            args);
+    }
+
+}
---
+++ src/test/java/com/lambdaschool/getjavaorders/GetjavaordersApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.getjavaorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetjavaordersApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
